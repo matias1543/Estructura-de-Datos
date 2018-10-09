7a5742c3f406e16fa9101e9aba21231c0cb89ff9
Matias J <matias_julian@outlook.com>
2018-10-09 00:21:33 -0300

    Cajero
    Proyecto Estructura de Datos.

---
+++ Cajero/Operaciones.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cajero
+{
+    class Operaciones
+    {
+        //Define las operaciones basicas del cajero
+        public double saldo = 0;
+        public double abono = 0;
+        public double retiro = 0;
+        public int pin = 1234;
+
+        public void mostrarSaldo(double dinero)
+        {
+            Console.Clear();
+            Console.WriteLine("***************************************");
+            Console.WriteLine("El saldo de su cuenta es de " + dinero);
+            Console.WriteLine("***************************************");
+        }
+
+        public double agregarBono(double aSaldo, double aAbono)
+        {
+            // Permite agregar efectivo a la cuenta
+            Console.Clear();
+            Console.WriteLine("Ingrese la cantidad a abonar: ");
+            aAbono = Convert.ToDouble(Console.ReadLine());
+            aSaldo += aAbono;
+
+            Console.WriteLine("Abono realizado con exito");
+            Console.WriteLine("");
+
+            return aSaldo;
+        }
+
+        public double retirarEfectivo(double aSaldo, double aRetiro)
+        {
+            // Permite retirar dinero de la cuenta
+            Console.Clear();
+            Console.WriteLine("Ingrese la cantidad a retirar: ");
+            aRetiro = Convert.ToDouble(Console.ReadLine());
+            aSaldo -= aRetiro;
+
+            if (aSaldo < 0)
+            {
+                aSaldo += aRetiro;
+                Console.WriteLine("No pudo realizarse la operacion");
+                Console.WriteLine("");
+            }
+            else if(aRetiro > 5000)
+            {
+                aSaldo += aRetiro;
+                Console.WriteLine("No puedo retirar mas de $5000");
+            }
+            else
+            {
+                Console.WriteLine("Operacion realizada con exito");
+            }
+            return aSaldo;
+        }
+
+        public int generarPIN(int aPIN)
+        {
+            //Permite generar un nuevo PIN
+            Console.Clear();
+            Console.WriteLine("Ingrese su nuevo PIN: ");
+            aPIN = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("PIN generado con exito");
+            Console.WriteLine("");
+            return aPIN;
+        }
+
+        public void generarRecibo(double aSaldo, double aAbono, double aRetiro)
+        {
+          int opcion;
+            Console.WriteLine("Desea recibir un recibo de la operacion actual?");
+            Console.WriteLine("1. Si");
+            Console.WriteLine("2. No");
+            opcion = Convert.ToInt32(Console.ReadLine());
+         
+        if(opcion == 1)
+        {
+            Console.Clear();
+            Console.WriteLine("El saldo actual de su cuenta es de " +aSaldo);
+            Console.WriteLine("Muchas gracias por utilizar nuestros servicios.");
+        }
+        else
+        {
+            Console.WriteLine("Muchas gracias por utilizar nuestros servicios.");
+        }
+    }
+}
+}
---
+++ Cajero/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cajero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones cajero = new Operaciones();
+            int opcion;
+
+            double cAbono = cajero.abono;
+            double cRetiro = cajero.retiro;
+            double cSaldo = cajero.saldo;
+            int cPIN = cajero.pin;
+            int prueba;
+
+            // Vista de la consola
+            Console.Title = "Cajero Automatico";
+            // Menu de presentacion
+            Console.WriteLine("Bienvenido al cajero");
+            Console.WriteLine("--------------------");
+            do
+            {
+                do
+                {
+                    Console.WriteLine("PIN");
+                    prueba = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                } while (cPIN != prueba);
+
+                Console.WriteLine("Seleccione una opcion por favor:");
+                Console.WriteLine("");
+                Console.WriteLine("1. Realizar un deposito");
+                Console.WriteLine("2. Realizar un retiro");
+                Console.WriteLine("3. Consultar saldo");
+                Console.WriteLine("4. Generar nuevo PIN");
+                Console.WriteLine("5 - Salir");
+                Console.WriteLine("");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch(opcion)
+                {
+                    case 1:
+                        cSaldo =  cajero.agregarBono(cSaldo, cAbono);
+                        cajero.generarRecibo(cSaldo, cAbono, cRetiro);
+                        Console.WriteLine("\n");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        cSaldo = cajero.retirarEfectivo(cSaldo, cRetiro);
+                        cajero.generarRecibo(cSaldo, cAbono, cRetiro);
+                        Console.WriteLine("\n");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        // Opcion consultar saldo
+                        cajero.mostrarSaldo(cSaldo);
+                        Console.WriteLine("\n");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 4:
+                        // Generar nuevo PIN
+                        cPIN = cajero.generarPIN(cPIN);
+                        Console.WriteLine("\n");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 5:
+                        // Salida del sistema
+                        Console.Clear();
+                        Console.WriteLine("Gracias por su preferencia");
+                        Console.WriteLine("Presione cualquier tecla para terminar");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        // Ninguna opcion valida seleccionada
+                        Console.WriteLine("Elija una opcion valida");
+                        Console.Clear();
+                        break;
+                }
+            } while (opcion != 5);
+
+        }
+    }
+}
